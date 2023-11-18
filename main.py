635abb5a3f4fd998384a92c92946d24ed2ddff81
qgarot <quentingarot06@gmail.com>
2023-11-18 12:13:04 +0100

    Suppression fichier inutile, renommage

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import functions
 if __name__ == "__main__":
     A, b = functions.define_linear_system(50, 10, True)
     x = functions.conjugate_gradient_method(A, b, 1e-12, 100, True)
-    functions.display_solution(x, "--", True)
+    functions.display_function(x, "--", True)
