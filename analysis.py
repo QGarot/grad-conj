94c39d549cbb22e31103f0b9231e90fd015d6f5f
qgarot <quentingarot06@gmail.com>
2023-11-18 12:47:16 +0100

    Premiers éléments d'analyse

---
+++ analysis.py
@@ -0,0 +1,42 @@
+import functions
+import matplotlib.pyplot as plt
+
+
+def multi_display_dimensions(dimensions: list[int], k: int) -> None:
+    """
+    Affiche sur un même graphique les solutions approchées du système linéaire en fonction de sa dimension
+    :param dimensions:
+    :param k: k fixé
+    :return:
+    """
+    for dim in dimensions:
+        A, b = functions.define_linear_system(dim, k)
+        x = functions.conjugate_gradient_method(A, b, 0.0001, 50)
+
+        nb_points = len(x)
+        delta = 1 / (nb_points - 1)
+        plt.plot([(i-1) * delta for i in range(1, nb_points+1)], x, ls="--", marker=".")
+
+    plt.grid(True)
+    plt.show()
+
+
+def multi_display_k(params: list[int], n: int) -> None:
+    """
+    Affiche sur un même graphique les solutions approchées du système linéaire en fonction du paramètre k
+    :param params:
+    :param n: dimension du système linéaire fixée
+    :return:
+    """
+    for k in params:
+        A, b = functions.define_linear_system(n, k)
+        x = functions.conjugate_gradient_method(A, b, 0.001, 50)
+
+        nb_points = len(x)
+        delta = 1 / (nb_points - 1)
+        plt.plot([(i-1) * delta for i in range(1, nb_points+1)], x, ls="--", marker=".", label="k = " + str(k))
+
+    plt.legend()
+    plt.title("Solutions du système linéaire Ax = b en fonction du paramètre k")
+    plt.grid(True)
+    plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import functions
+import analysis
 
 
 if __name__ == "__main__":
-    A, b = functions.define_linear_system(50, 10, True)
-    x = functions.conjugate_gradient_method(A, b, 1e-12, 100, True)
-    functions.display_function(x, "--", True)
+    # A, b = functions.define_linear_system(50, 10, True)
+    # x = functions.conjugate_gradient_method(A, b, 1e-12, 100, True)
+    # functions.display_function(x, "--", True)
+    analysis.multi_display_k([-1, 0, 5, 10, 15, 100], 50)
+
